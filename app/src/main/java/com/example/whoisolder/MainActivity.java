2cffd2d8ba3abf8cfd01e5930c1ae323a561bdb6
Marcos Martinez <marcos.martinez.gonzalo@gmail.com>
2023-04-25 19:02:10 +0200

    API 26

--- app/src/main/java/com/example/whoisolder/MainActivity.java
+++ app/src/main/java/com/example/whoisolder/MainActivity.java
@@ -6,9 +6,16 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
